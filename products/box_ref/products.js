401af2cdb15655a5f6f5f09f3c930e7598889755
Yryskul <mail.iriskul@gmail.com>
2022-03-15 00:25:26 +0300

    product 1.5

--- products/box_ref/products.js
+++ products/box_ref/products.js
@@ -69,6 +69,309 @@ let data = [{
             },
         ]
     },
+    {
+        data: [{
+                img: "100_02_008",
+                type: "Recessed Door Lock With Key Ø16 mm",
+                info: {
+                    "Galvanized": "100.02.007",
+                    "Stainless Steel": "100.02.008"
+                }
+            },
+            {
+                img: "100_02_012",
+                type: "Small Recessed Door Lock With Key Ø16 mm",
+                info: {
+                    "Galvanized": "100.02.011",
+                    "Stainless Steel": "100.02.012"
+                }
+            },
+            {
+                img: "100_03_002",
+                type: "Cam & Keeper",
+                info: {
+                    "Galvanized": "100.03.001",
+                    "Stainless Steel": "100.03.002"
+                }
+            },
+            {
+                img: "100_02_022",
+                type: "Recessed Handle Lock With Key",
+                info: {
+                    "Galvanized": "100.02.021",
+                    "Stainless Steel": "100.02.022"
+                }
+            },
+        ]
+    },
+    {
+        data: [{
+                img: "100_02_024",
+                type: "Recessed Handle Lock With Key",
+                info: {
+                    "Galvanized": "100.02.023",
+                    "Stainless Steel": "100.02.024"
+                }
+            },
+            {
+                img: "100_02_026",
+                type: "Recessed Handle Lock",
+                info: {
+                    "Galvanized": "100.02.025",
+                    "Stainless Steel": "100.02.026"
+                }
+            },
+            {
+                img: "100_02_027",
+                type: "Recessed Handle Lock",
+                info: {
+                    "": "100.02.027"
+                }
+            },
+            {
+                img: "100_02_014",
+                type: "TIR Lever KIT Ø27 mm",
+                info: {
+                    "Galvanized": "100.02.013",
+                    "Stainless Steel": "100.02.014"
+                }
+            },
+        ]
+    },
+    {
+        data: [{
+                img: "100_04_002",
+                type: "Lever Retainer Ø27 mm",
+                info: {
+                    "Galvanized": "100.04.001",
+                    "Stainless Steel": "100.04.002"
+                }
+            },
+            {
+                img: "100_04_004",
+                type: "Lever Retainer Ø27 mm",
+                info: {
+                    "Galvanized": "100.04.003",
+                    "Stainless Steel": "100.04.004"
+                }
+            },
+            {
+                img: "100_04_006",
+                type: "Big Guide Plate Ø27 mm",
+                info: {
+                    "Galvanized": "100.04.005",
+                    "Stainless Steel": "100.04.006"
+                }
+            },
+            {
+                img: "100_04_008",
+                type: "Small Guide Plate Ø27 mm",
+                info: {
+                    "Galvanized": "100.04.007",
+                    "Stainless Steel": "100.04.008"
+                }
+            },
+        ]
+    },
+    {
+        data: [{
+                img: "100_03_004",
+                type: "Cam & Keeper Ø27 mm",
+                info: {
+                    "Galvanized": "100.03.003",
+                    "Stainless Steel": "100.03.004"
+                }
+            },
+            {
+                img: "100_03_006",
+                type: "Cam & Keeper Ø27 mm",
+                info: {
+                    "Galvanized": "100.03.005",
+                    "Stainless Steel": "100.03.006"
+                }
+            },
+            {
+                img: "100_03_008",
+                type: "Big Cam & Keeper Ø27 mm",
+                info: {
+                    "Galvanized": "100.03.007",
+                    "Stainless Steel": "100.03.008"
+                }
+            },
+            {
+                img: "100_03_010",
+                type: "Cam & Keeper Ø22 mm",
+                info: {
+                    "Galvanized": "100.03.009",
+                    "Stainless Steel": "100.03.010"
+                }
+            },
+        ]
+    },
+    {
+        data: [{
+                img: "100_03_012",
+                type: "Cam & Keeper Ø22 mm",
+                info: {
+                    "Galvanized": "100.03.011",
+                    "Stainless Steel": "100.03.012"
+                }
+            },
+            {
+                img: "100_03_014",
+                type: "Cam & Keeper Ø27 mm",
+                info: {
+                    "Galvanized": "100.03.013",
+                    "Stainless Steel": "100.03.014"
+                }
+            },
+            {
+                img: "100_02_020",
+                type: "TIR Lever KIT Ø22 mm",
+                info: {
+                    "Galvanized": "100.02.019",
+                    "Stainless Steel": "100.02.020"
+                }
+            },
+            {
+                img: "100_04_010",
+                type: "Lever Mount Ø22 mm",
+                info: {
+                    "Galvanized": "100.04.009",
+                    "Stainless Steel": "100.04.010"
+                }
+            },
+        ]
+    },
+    {
+        data: [{
+                img: "100_04_012",
+                type: "Lever Retainer Ø22 mm",
+                info: {
+                    "Galvanized": "100.04.011",
+                    "Stainless Steel": "100.04.012"
+                }
+            },
+            {
+                img: "100_04_014",
+                type: "Big Guide Plate Ø22 mm",
+                info: {
+                    "Galvanized": "100.04.013",
+                    "Stainless Steel": "100.04.014"
+                }
+            },
+            {
+                img: "153_01_09_011",
+                type: "Big Guide Plate Ø22 mm",
+                info: {
+                    "Galvanized": "153.01.09.012",
+                    "Stainless Steel": "153.01.09.011"
+                }
+            },
+            {
+                img: "100_05_002",
+                type: "Door Holder",
+                info: {
+                    "Galvanized": "100.05.001",
+                    "Stainless Steel": "100.05.002"
+                }
+            },
+        ]
+    },
+    {
+        data: [{
+                img: "100_05_003",
+                type: "Rear Door Retainer (Galvanized)",
+                info: {
+                    "Galvanized": "100.05.003"
+                }
+            },
+            {
+                img: "100_05_005",
+                type: "Rear Door Retainer (Galvanized)",
+                info: {
+                    "Galvanized": "100.05.005",
+                }
+            },
+            {
+                img: "100_01_013",
+                type: "Sideboard Hinge (Galvanized)",
+                info: {
+                    "Galvanized": "100.01.013",
+                }
+            },
+            {
+                img: "100_01_015",
+                type: "Sideboard Hinge (Galvanized)",
+                info: {
+                    "Galvanized": "100.01.015"
+                }
+            },
+        ]
+    },
+    {
+        data: [{
+                img: "100_01_017",
+                type: "Sideboard Hinge (Galvanized)",
+                info: {
+                    "Galvanized": "100.01.017"
+                }
+            },
+            {
+                img: "100_01_019",
+                type: "Hinge of Box",
+                info: {
+                    "Code": "100.01.019",
+                }
+            },
+            {
+                img: "100_01_021",
+                type: "Hinge of Box",
+                info: {
+                    "Code": "100.01.021",
+                }
+            },
+            {
+                img: "100_07_001",
+                type: "Fastener (Galvanized)",
+                info: {
+                    "Galvanized": "100.07.001"
+                }
+            },
+        ]
+    },
+    {
+        data: [{
+                img: "100_07_003",
+                type: "Recessed Fastener (Galvanized)",
+                info: {
+                    "Galvanized": "100.07.003"
+                }
+            },
+            {
+                img: "100_07_005",
+                type: "Adjustable Safety Bolt Fastener (Galvanized)",
+                info: {
+                    "Code": "100.07.005",
+                }
+            },
+            {
+                img: "100_07_013",
+                type: "Fastener Pocket (Galvanized)",
+                info: {
+                    "30mm": "100.07.011",
+                    "36mm": "100.07.013",
+                }
+            },
+            {
+                img: "100_07_009",
+                type: "T-Piece Long (Galvanized) 95mm",
+                info: {
+                    "Galvanized": "100.07.009"
+                }
+            },
+        ]
+    },
 ]
 
 function moreBoxProduct() {
@@ -93,7 +396,7 @@ function moreBoxProduct() {
 
 
             infos.forEach((key, i) => {
-                infoHtml += '<strong>' + key + ':</strong> ' + d.info[key] + ((i == infos.length - 1) ? '' : '<br>');
+                infoHtml += '<strong>' + key + (key ? ':' : '') + '</strong> ' + d.info[key] + ((i == infos.length - 1) ? '' : '<br>');
             })
 
             let textHtml = '<div>\
