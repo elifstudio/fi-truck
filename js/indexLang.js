dfc70a32c92c5573c0db1a9a352d9a3798f4cd42
Yryskul <mail.iriskul@gmail.com>
2022-04-03 18:38:16 +0300

    index lang is done

--- js/indexLang.js
+++ js/indexLang.js
@@ -7,7 +7,223 @@ function getPageLangs() {
             de: "Neue Produkte",
             ar: "منتجات جديدة",
             fr: "Nouveaux produits"
-        }
+        },
+        "future_innovations": {
+            en: "Future Innovations",
+            tr: "",
+            ru: "",
+            de: "",
+            ar: "",
+            fr: ""
+        },
+        "future_i_ltd": {
+            en: "Future Innovations Ltd is manufacturer and partner of leading manufacturers of components <br> for refrigerated boxes, trailers, semi – trailers and chassis.",
+            tr: "",
+            ru: "",
+            de: "",
+            ar: "",
+            fr: ""
+        },
+        "accessories": {
+            en: "Accessories",
+            tr: "",
+            ru: "",
+            de: "",
+            ar: "",
+            fr: ""
+        },
+        "accessories_txt": {
+            en: "«Future Innovation Ltd» offers only the highest quality accessories for carrosseries and specialized equipment.",
+            tr: "",
+            ru: "",
+            de: "",
+            ar: "",
+            fr: ""
+        },
+        "about_1": {
+            en: "FUTURE INNOVATIONS DIS TIC. LTD. STI. was established in ISTANBUL / TURKEY in 2012.Since then we have been developing everyday. Today we are proud to collaborate with leading manufacturers of truck body builders, trailers, semi trailers, firefighting trucks and commercial vehicles Worldwide.",
+            tr: "",
+            ru: "",
+            de: "",
+            ar: "",
+            fr: ""
+        },
+        "about_2": {
+            en: "We produce (if not everything) and supply ALL kinds of parts and accessories for all types of: semi-trailers, dry boxes (dry vans), reefer boxes (refrigerated boxes), reefer semi-trailers, carroseries, tankers, curtain-side trailers, commercial vehicles, container shipping semi-trailers,commercial vehicles, bodies, trailers, firefighting trucks, emergency response vehicles, ambulances, vans, caravans.",
+            tr: "",
+            ru: "",
+            de: "",
+            ar: "",
+            fr: ""
+        },
+        "about_3": {
+            en: "We can offer you",
+            tr: "",
+            ru: "",
+            de: "",
+            ar: "",
+            fr: ""
+        },
+        "about_li_1": {
+            en: "Truck, semi trailer, reefer and dry box body parts:hinges, locks, handles, external locks, push handles, door retainers, side protection bars,interior lights, truck ladders, truck partition walls, truck door seals(gaskets or profiles),truck body aluminum profiles, twist locks, truck body casting parts, side pillars, sliding roof systems, recessed handles, cam and keepers, truck sandwich panels, reinforced fiberglass sheets",
+            tr: "",
+            ru: "",
+            de: "",
+            ar: "",
+            fr: ""
+        },
+        "about_li_2": {
+            en: "Firefighting vehicle (truck) parts: Roller shutters, roll up doors, rear aluminum ladders, zone lights, firemen seats, clamps, window frames, hinges, door handles, gaskets, profiles and seals, warning lights and siren systems, strobe lights, heavy duty slides, steps.",
+            tr: "",
+            ru: "",
+            de: "",
+            ar: "",
+            fr: ""
+        },
+        "about_li_3": {
+            en: "Bus bodyworks parts: Ventilation hatches, luggage racks, service sets, interior lights, VIP seats, bus seats, passenger seats, aluminum profiles, gaskets, window frames, curtain sets.",
+            tr: "",
+            ru: "",
+            de: "",
+            ar: "",
+            fr: ""
+        },
+        "new_model_hingle": {
+            en: "New Model Hingle",
+            tr: "",
+            ru: "",
+            de: "",
+            ar: "",
+            fr: ""
+        },
+        "hing_galv_stain": {
+            en: "Hingle (galv. / Stainless steel.)",
+            tr: "",
+            ru: "",
+            de: "",
+            ar: "",
+            fr: ""
+        },
+        "product_code": {
+            en: "Product Code",
+            tr: "",
+            ru: "",
+            de: "",
+            ar: "",
+            fr: ""
+        },
+        "galvanized": {
+            en: "Galvanized",
+            tr: "",
+            ru: "",
+            de: "",
+            ar: "",
+            fr: ""
+        },
+        "stainless": {
+            en: "Stainless",
+            tr: "",
+            ru: "",
+            de: "",
+            ar: "",
+            fr: ""
+        },
+        "packing_quantity": {
+            en: "Packing Quantity (pcs)",
+            tr: "",
+            ru: "",
+            de: "",
+            ar: "",
+            fr: ""
+        },
+        "read_more": {
+            en: "Read more...",
+            tr: "",
+            ru: "",
+            de: "",
+            ar: "",
+            fr: ""
+        },
+        "new_model_handle_lock": {
+            en: "New Model Handle Lock with Key",
+            tr: "",
+            ru: "",
+            de: "",
+            ar: "",
+            fr: ""
+        },
+        "model_handle_lock": {
+            en: "Model Handle Lock with Key Ø27mm",
+            tr: "",
+            ru: "",
+            de: "",
+            ar: "",
+            fr: ""
+        },
+        "interior_lights": {
+            en: "Interior lights (Power Led)",
+            tr: "",
+            ru: "",
+            de: "",
+            ar: "",
+            fr: ""
+        },
+        "interior_wall": {
+            en: "Interior wall systems for trucks",
+            tr: "",
+            ru: "",
+            de: "",
+            ar: "",
+            fr: ""
+        },
+        "istanbul_loc": {
+            en: "ISTANBUL LOCATION",
+            tr: "",
+            ru: "",
+            de: "",
+            ar: "",
+            fr: ""
+        },
+        "address": {
+            en: "Address",
+            tr: "",
+            ru: "",
+            de: "",
+            ar: "",
+            fr: ""
+        },
+        "email": {
+            en: "Email",
+            tr: "",
+            ru: "",
+            de: "",
+            ar: "",
+            fr: ""
+        },
+        "phone": {
+            en: "Phone",
+            tr: "",
+            ru: "",
+            de: "",
+            ar: "",
+            fr: ""
+        },
+        "skype": {
+            en: "Skype",
+            tr: "",
+            ru: "",
+            de: "",
+            ar: "",
+            fr: ""
+        },
+        "ankara_loc": {
+            en: "ANKARA LOCATION",
+            tr: "",
+            ru: "",
+            de: "",
+            ar: "",
+            fr: ""
+        },
     }
 
     return langs;
